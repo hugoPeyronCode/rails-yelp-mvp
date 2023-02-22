6bcec1a3b7db60bb9efaddce2fe3f4f525781acd
Hugo Peyron <hugopeyron@gmail.com>
2023-02-22 12:31:53 +0100

    models part done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/edit'
+  get 'reviews/update'
+  get 'reviews/destroy'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/edit'
+  get 'restaurants/update'
+  get 'restaurants/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
